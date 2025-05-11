e16731ee62d9fbe4c1337316d3f2f6595c14bea3
Shifatu Halimah Olamide <shifatuhalimah20@gmail.com>
2025-05-11 12:40:34 +0100

    Changed the ScrollToTop.jsx folder to ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Loader from "./Components/Loader/Loader";
 import About from "./Components/About/About";
-import ScrollToTop from "./Components/ScrollToTop.jsx/ScrollToTop";
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
 import "./App.css";
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
